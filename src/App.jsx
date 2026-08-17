6f2339f60375e271e268dfdcba0f5793dcca3e7d
harsha-lv <harsha.2006@gmail.com>
2026-08-17 21:46:07 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
-// App.jsx
-
-import { useState } from "react";
+import { Link } from "react-router";
 
 const App = () => {
+  return (
+    <div>
+      <h1>Hello from the main page of the app!</h1>
+      <p>Here are some examples of links to other pages</p>
+      <nav>
+        <ul>
+          <li>
+            <Link to="profile">Profile page</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default App;
+
---
+++ src/routes.jsx
@@ -0,0 +1,18 @@
+import App from "./App";
+import Profile from "./Profile";
+import ErrorPage from "./ErrorPage";
+
+const routes = [
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "profile/:name",
+    element: <Profile />,
+  },
+];
+
+export default routes;
+
